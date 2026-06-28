e4498b72545d6b33c1e46d8e0023b1dec89637de
Mohd Valeed <mohdvaleedhasan@gmail.com>
2026-06-29 04:53:14 +0530

    Fix email problem

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
